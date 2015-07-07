647eac91e378473c6aa45d6808e2c74e480de019
Jur Balledux <j.balledux@simgroep.nl>
2015-07-07 11:16:28 +0200

    Publish domain events to queue

--- src/EventHandling/PublishMessageToQueue.php
+++ src/EventHandling/PublishMessageToQueue.php
@@ -2,18 +2,35 @@
 
 namespace Simgroep\EventSourcing\EventHandling;
 
+use Broadway\Domain\DomainEventStream;
 use Broadway\Domain\DomainMessage;
 use Broadway\EventHandling\EventListenerInterface;
+use Simgroep\EventSourcing\Messaging\GenericMessage;
+use Simgroep\EventSourcing\Messaging\Queue;
 
 class PublishMessageToQueue implements EventListenerInterface
 {
+    /**
+     * @var Queue
+     */
+    private $queue;
 
+    /**
+     * @param Queue $queue
+     */
+    public function __construct(Queue $queue)
+    {
+        $this->queue = $queue;
+    }
 
     /**
      * @param DomainMessage $domainMessage
      */
     public function handle(DomainMessage $domainMessage)
     {
-
+        $this->queue->publish(new GenericMessage(
+            $domainMessage->getId(),
+            new DomainEventStream(array($domainMessage->getPayload()))
+        ));
     }
 }
