e5d1d36f7e902859d702005d2a76eaf836b63a32
Leon Weemen <l.weemen@simgroep.nl>
2015-07-10 14:17:09 +0200

    renamed class

--- src/EventSourcingBundle/ReadModel/RepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Simgroep\EventSourcing\EventSourcingBundle\ReadModel;
-
-use Broadway\ReadModel\RepositoryInterface as BroadwayRepositoryInterface;
-
-interface RepositoryInterface extends BroadwayRepositoryInterface
-{
-    public function removeAll();
-}
