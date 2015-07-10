08ea8f0f328029fae5745fd65631e980dd902fb8
Leon Weemen <l.weemen@simgroep.nl>
2015-07-10 15:19:26 +0200

    resolved change from spray

--- src/EventSourcingBundle/Serializer/DomainMessageSerializer.php
+++ src/EventSourcingBundle/Serializer/DomainMessageSerializer.php
@@ -10,21 +10,37 @@ class DomainMessageSerializer extends AbstractObjectSerializer
     public function __construct()
     {
         parent::__construct(
-            function($subject, array &$data, SerializerInterface $serializer) {
-                $data['playhead'] = (int) $subject->playhead;
-                $data['metadata'] = isset($subject->metadata) ? $serializer->serialize($subject->metadata) : null;
-                $data['id'] = (string) $subject->id;
-                $data['recordedOn'] = isset($subject->recordedOn) ? $serializer->serialize($subject->recordedOn) : null;
-                $data['payload'] = isset($subject->payload) ? $serializer->serialize($subject->payload) : null;
-            },
-            function($subject, array &$data, SerializerInterface $serializer) {
-                $subject->playhead = (int) $data['playhead'];
-                $subject->metadata = isset($data['metadata']) ? $serializer->deserialize('Broadway\Domain\Metadata', $data['metadata']) : null;
-                $subject->id = (string) $data['id'];
-                $subject->recordedOn = isset($data['recordedOn']) ? $serializer->deserialize('Broadway\Domain\DateTime', $data['recordedOn']) : null;
-                $subject->payload = isset($data['payload']) ? $serializer->deserialize(null, $data['payload']) : null;
-            },
             'Broadway\Domain\DomainMessage'
         );
     }
+
+    /**
+     * @return callable
+     */
+    protected function bindSerializer()
+    {
+        return function($subject, array &$data, SerializerInterface $serializer) {
+            $data['playhead'] = (int) $subject->playhead;
+            $data['metadata'] = isset($subject->metadata) ? $serializer->serialize($subject->metadata) : null;
+            $data['id'] = (string) $subject->id;
+            $data['recordedOn'] = isset($subject->recordedOn) ? $serializer->serialize($subject->recordedOn) : null;
+            $data['payload'] = isset($subject->payload) ? $serializer->serialize($subject->payload) : null;
+        };
+    }
+
+    /**
+     * @return callable
+     */
+    protected function bindDeserializer()
+    {
+        return function($subject, array &$data, SerializerInterface $serializer) {
+            $subject->playhead = (int) $data['playhead'];
+            $subject->metadata = isset($data['metadata']) ? $serializer->deserialize('Broadway\Domain\Metadata', $data['metadata']) : null;
+            $subject->id = (string) $data['id'];
+            $subject->recordedOn = isset($data['recordedOn']) ? $serializer->deserialize('Broadway\Domain\DateTime', $data['recordedOn']) : null;
+            $subject->payload = isset($data['payload']) ? $serializer->deserialize(null, $data['payload']) : null;
+        };
+    }
+
+
 }
