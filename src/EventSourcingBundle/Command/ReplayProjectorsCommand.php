a72f483f3d2f666a538971dcf6022280c3370e1e
Jur Balledux <j.balledux@simgroep.nl>
2015-09-02 12:35:26 +0200

    Implemented multithreading projection

--- src/EventSourcingBundle/Command/ReplayProjectorsCommand.php
+++ src/EventSourcingBundle/Command/ReplayProjectorsCommand.php
@@ -2,37 +2,89 @@
 
 namespace Simgroep\EventSourcing\EventSourcingBundle\Command;
 
+use Generator;
+use React\ChildProcess\Process;
+use React\EventLoop\Factory;
+use RuntimeException;
+use Simgroep\EventSourcing\EventSourcingBundle\Infrastructure\Replay;
 use Simgroep\EventSourcing\EventSourcingBundle\ProjectorRegistry\ProjectorRegistry;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ReplayProjectorsCommand extends ContainerAwareCommand
 {
+    /**
+     * @var float
+     */
+    private $time;
+
+    /**
+     * @var string
+     */
+    private $perSecond;
 
     protected function configure()
     {
         $this
             ->setName('simgroep:eventsourcing:events:replay')
-            ->setDescription('Replays events to rebuild projections')
-        ;
+            ->setDescription('Replays events to rebuild projections');
 
         $this->addArgument(
             'projector',
             InputArgument::REQUIRED,
             'The name of the projector (comma separated for multiple projectors) or "all" for all projectors'
         );
+        $this->addOption(
+            'stream',
+            'stream',
+            InputOption::VALUE_REQUIRED,
+            'The stream id to project'
+        );
+        $this->addOption(
+            'threads',
+            'threads',
+            InputOption::VALUE_REQUIRED,
+            'The number of threads to dispatch',
+            4
+        );
     }
 
 
+    /**
+     * Decides what to execute based on input arguments.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var \Simgroep\EventSourcing\EventSourcingBundle\Infrastructure\Replay $eventStore */
-        /** @var \Simgroep\EventSourcing\EventSourcingBundle\ProjectorRegistry\ProjectorRegistry $projectorRegistry */
+        $stream = $input->getOption('stream');
+
+        if ( ! $stream) {
+            $this->executePreparation($input, $output);
+            $this->executeStreamManager($input, $output);
+        } else {
+            $this->executeStreamHandler($stream, $input, $output);
+        }
+    }
+
+    /**
+     * Prepare readmodels for projection (clears the data).
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function executePreparation(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Replay $eventStore */
+        /** @var ProjectorRegistry $projectorRegistry */
         $eventStore         = $this->getContainer()->get('sim.event_store.replay');
         $projectorRegistry  = $this->getContainer()->get('sim.projector.registry');
-
 
         $projectorsToHandle  = $this->generateProjectorList(
             $input->getArgument('projector'),
@@ -40,25 +92,120 @@ class ReplayProjectorsCommand extends ContainerAwareCommand
         );
 
         $output->writeln("\n<info>Deleting old projections</info>");
-
         $this->prepareProjectorsForRebuilding($projectorRegistry, $projectorsToHandle);
+        $output->writeln("--------------------------------------------------------------------------------------------------------------------------------------------");
+    }
 
-        $output->writeln('<info>Start building new projections.</info>');
+    /**
+     * Dispatches projector thread per event stream.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function executeStreamManager(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Generator $streams */
+        $streams = $this->getContainer()->get('sim.event_store.replay')->streams();
+        $loop = Factory::create();
+        $threads = 0;
+        $iteration = 0;
+
+        $loop->addPeriodicTimer(.001, function($timer) use ($loop, $streams, &$threads, &$iteration, $input, $output) {
+            if ($threads >= $input->getOption('threads')) {
+                return;
+            }
+
+            if ( ! $streams->valid()) {
+                $loop->stop();
+            }
+
+            $threads++;
+            $stream = $streams->current();
+            $output->writeln(sprintf(
+                '<info>Dispatching thread for event stream %s</info>',
+                $stream
+            ));
+
+            $process = new Process(sprintf(
+                'app/console simgroep:eventsourcing:events:replay %s --stream %s',
+                $input->getArgument('projector'),
+                $stream
+            ));
+            $process->on('exit', function($exitCode, $termSignal) use (&$threads, $output) {
+                $threads--;
+                if ($exitCode !== 0) {
+                    $output->writeln(sprintf(
+                        '<error>Process ended with code %s</error>',
+                        $exitCode
+                    ));
+                }
+            });
+
+            $process->start($timer->getLoop());
+            if ($input->getOption('verbose')) {
+                $process->stdout->on('data', function($message) use ($output) {
+                    $output->write($message);
+                });
+            }
+
+            $output->writeln(
+                sprintf(
+                    '<info>Stream %s | %s streams/sec</info>',
+                    str_pad(++$iteration, 10, ' ', STR_PAD_LEFT),
+                    str_pad($this->perSecond($iteration), 6, ' ', STR_PAD_LEFT)
+                )
+            );
+
+            $streams->next();
+        });
+
+
+        $loop->run();
+    }
+
+    /**
+     * Project events for given $stream.
+     *
+     * @param string $stream
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function executeStreamHandler($stream, InputInterface $input, OutputInterface $output)
+    {
+        /** @var Replay $eventStore */
+        /** @var ProjectorRegistry $projectorRegistry */
+        $eventStore         = $this->getContainer()->get('sim.event_store.replay');
+        $projectorRegistry  = $this->getContainer()->get('sim.projector.registry');
+
+        $projectorsToHandle  = $this->generateProjectorList(
+            $input->getArgument('projector'),
+            $projectorRegistry
+        );
+
+        $output->writeln(sprintf(
+            '<info>Projecting stream %s</info>',
+            $stream
+        ));
         $output->writeln("--------------------------------------------------------------------------------------------------------------------------------------------");
 
-        $eventStore->replay(function($domainMessage) use ($projectorRegistry, $projectorsToHandle, $output) {
+        $i = 0;
+        $eventStore->replay($stream, function($domainMessage) use ($projectorRegistry, $projectorsToHandle, $output, &$i) {
             /** @var \Broadway\Domain\DomainMessage $domainMessage */
             /** @var \Broadway\ReadModel\ProjectorInterface $projector */
             foreach ($projectorRegistry as $serviceId => $projector) {
 
                 if (!in_array($serviceId, $projectorsToHandle)) {
-                    continue;                
+                    continue;
                 }
- 
+
                 $projector->handle($domainMessage);
                 $output->writeln(
                     sprintf(
-                        '<comment>-Replayed: %s: %s on projector %s</comment>',
+                        '<comment>- Replayed: %s: %s on projector %s</comment>',
                         $domainMessage->getId(),
                         $domainMessage->getType(),
                         $serviceId
@@ -70,6 +217,28 @@ class ReplayProjectorsCommand extends ContainerAwareCommand
         });
 
         $output->writeln("<info>Rebuilding projections completed.</info>\n");
+    }
+
+    /**
+     * Calculates iterations per second.
+     *
+     * @param integer $iteration
+     *
+     * @return string
+     */
+    protected function perSecond($iteration)
+    {
+        if (null === $this->time) {
+            $this->time = microtime(true);
+            $this->perSecond = '...';
+        }
+
+        if (0 === $iteration % 50) {
+            $this->perSecond = (string) round(1 / ((microtime(true) - $this->time) / 50), 2);
+            $this->time = microtime(true);
+        }
+
+        return $this->perSecond;
     }
 
     /**
--- src/EventSourcingBundle/Infrastructure/Replay.php
+++ src/EventSourcingBundle/Infrastructure/Replay.php
@@ -6,59 +6,130 @@ use Broadway\Domain\DateTime;
 use Broadway\Domain\DomainMessage;
 use Broadway\Serializer\SerializerInterface;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Driver\Statement;
+use Generator;
 
 class Replay
 {
+    /**
+     * @var Connection
+     */
     private $connection;
-    private $serializerMetadata;
-    private $serializerPayload;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $metadataSerializer;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $payloadSerializer;
+
+    /**
+     * @var string
+     */
     private $tableName;
-    private $loadStatement;
+
+    /**
+     * @var Statement
+     */
+    private $replayStatement;
+
+    /**
+     * @var Statement
+     */
+    private $streamsStatement;
 
     /**
      * @param Connection $connection
-     * @param SerializerInterface $serializer
+     * @param SerializerInterface $metadataSerializer
+     * @param SerializerInterface $payloadSerializer
      * @param string $tableName
      */
     public function __construct(
         Connection $connection,
-        SerializerInterface  $serializerMetadata,
-        SerializerInterface $serializerPayload,
+        SerializerInterface  $metadataSerializer,
+        SerializerInterface $payloadSerializer,
         $tableName = 'events')
     {
         $this->connection           = $connection;
-        $this->serializerMetadata   = $serializerMetadata;
-        $this->serializerPayload    = $serializerPayload;
+        $this->metadataSerializer   = $metadataSerializer;
+        $this->payloadSerializer    = $payloadSerializer;
         $this->tableName            = $tableName;
     }
 
     /**
-     * @param $callback
+     * Get a list of all streams.
+     *
+     * @return Generator
      */
-    public function replay($callback)
+    public function streams()
     {
-        $statement = $this->prepareLoadStatement();
+        $statement = $this->prepareStreamsStatement();
+        $statement->execute();
+
+        while ($row = $statement->fetch()) {
+            yield $row['uuid'];
+        }
+    }
+
+    /**
+     * Prepare the streams statement.
+     *
+     * @return Statement
+     * @throws DBALException
+     */
+    private function prepareStreamsStatement()
+    {
+        if (null === $this->streamsStatement) {
+            $query = sprintf(
+                'SELECT DISTINCT uuid
+                   FROM %s
+                  ORDER BY id ASC',
+                $this->tableName
+            );
+            $this->streamsStatement = $this->connection->prepare($query);
+        }
+        return $this->streamsStatement;
+    }
+
+    /**
+     * Replay events from a specific stream.
+     *
+     * @param string $stream
+     * @param callable $callback
+     */
+    public function replay($stream, callable $callback)
+    {
+        $statement = $this->prepareReplayStatement();
+        $statement->bindValue(1, $stream);
         $statement->execute();
 
         while ($row = $statement->fetch()) {
             $callback($this->deserializeEvent($row));
         }
-
     }
 
     /**
-     * @return \Doctrine\DBAL\Driver\Statement
-     * @throws \Doctrine\DBAL\DBALException
+     * @return Statement
+     *
+     * @throws DBALException
      */
-    protected function prepareLoadStatement()
+    private function prepareReplayStatement()
     {
-        if (null === $this->loadStatement) {
-            $query = 'SELECT uuid, playhead, metadata, payload, recorded_on
-                FROM ' . $this->tableName . '
-                ORDER BY id ASC';
-            $this->loadStatement = $this->connection->prepare($query);
+        if (null === $this->replayStatement) {
+            $query = sprintf(
+                'SELECT uuid, playhead, metadata, payload, recorded_on
+                   FROM %s
+                  WHERE uuid = ?
+                  ORDER BY playhead ASC',
+                $this->tableName
+            );
+            $this->replayStatement = $this->connection->prepare($query);
         }
-        return $this->loadStatement;
+        return $this->replayStatement;
     }
 
     /**
@@ -73,8 +144,8 @@ class Replay
         return new DomainMessage(
             $row['uuid'],
             $row['playhead'],
-            $this->serializerMetadata->deserialize($metadata, $metadata['payload']),
-            $this->serializerPayload->deserialize($payload, $payload['payload']),
+            $this->metadataSerializer->deserialize($metadata, $metadata['payload']),
+            $this->payloadSerializer->deserialize($payload, $payload['payload']),
             DateTime::fromString($row['recorded_on'])
         );
     }
